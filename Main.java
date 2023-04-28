b6da5efb27188f401c51f623f9ed6283977839b3
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2023-04-27 22:00:33 -0300

    ADO V1

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public void menu() {
+        System.out.println("1 - Adicionar produto");
+        System.out.println("2 - Remover produto");
+        System.out.println("3 - Listar Produtos");
+        System.out.println("4 - Quantidade de produtos em estoque");
+        System.out.println("5 - Capital em estoque");
+        System.out.println();
+    }
+}
